9d0dc2e84a94b52eb069961bddcc0ba2b8e42db5
Maxwell Mullin <mullinmax@gmail.com>
2023-06-09 17:46:22 +0000

    better markdown generation (#26)
    Reviewed-on: https://gitea.doze.dev/maxwell/purrs/pulls/26
    Co-authored-by: Maxwell Mullin <mullinmax@gmail.com>
    Co-committed-by: Maxwell Mullin <mullinmax@gmail.com>

--- generate_docs.py
+++ generate_docs.py
@@ -1,21 +1,35 @@
 import os
 import pkgutil
 import subprocess
+import shutil
 
 SRC_DIR = 'src'
 
 def generate_documentation(module_name):
+    # Remove 'src/' prefix from module_name
+    module_name = module_name.replace('src/', '')
     output_file = os.path.join('docs', f'{module_name}.md')
     with open(output_file, 'w') as f:
         subprocess.run(['pydoc-markdown', '-I', SRC_DIR, '-m', module_name, '--render-toc'], stdout=f)
+    # Return the filename of the generated documentation for use in the index
+    return output_file
 
 def main():
-    os.makedirs('docs', exist_ok=True)
+    docs_dir = os.path.join(os.getcwd(), 'docs')
+    # If docs directory exists, remove it and all its contents
+    if os.path.exists(docs_dir):
+        shutil.rmtree(docs_dir)
+    # Recreate docs directory
+    os.makedirs(docs_dir, exist_ok=True)
+    index_file = os.path.join('docs', 'index.md')
 
-    for finder, name, ispkg in pkgutil.iter_modules([SRC_DIR]):
-        if name == '__init__':
-            continue
-        generate_documentation(name)
+    with open(index_file, 'w') as index:
+        for finder, name, ispkg in pkgutil.iter_modules([SRC_DIR]):
+            if name == '__init__':
+                continue
+            output_file = generate_documentation(name)
+            # Write a link to the generated documentation in the index file
+            index.write(f'- [{name}]({output_file})\n')
 
 if __name__ == '__main__':
     main()
