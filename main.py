3350ead93863885783a5080d700fbd74df41185e
Maxwell Mullin <mullinmax@gmail.com>
2023-06-21 23:02:42 -0400

    super basic webpage

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+# app.py
+
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from src.database.base import Base  # Import the shared Base object
 from src.database.FeedItem import FeedItem  # This must come after importing Base
 from src.ingest.RSSFeed import RSSFeed
 
-engine = create_engine('sqlite:///rss.sqlite.')
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///rss.sqlite')
 Session = sessionmaker(bind=engine)
-
-Base.metadata.create_all(engine)  # This will now create tables for all models that inherit from Base
-
 session = Session()
 
-feed = RSSFeed('https://www.reddit.com/r/d100/.rss')
-items = feed.get_items()
+@app.route("/")
+def home():
+    feed = RSSFeed('https://www.reddit.com/r/d100/.rss')
+    items = feed.get_items()
 
-for item in items:  # Make sure to add items one by one, not the entire list
-    session.add(item)
-session.commit()
+    for item in items:  # Make sure to add items one by one, not the entire list
+        session.add(item)
+    session.commit()
+
+    return render_template('index.html', items=items)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
