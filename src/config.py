ce6615e23e54811f62a9cc50041667610b5bb71d
Maxwell Mullin <mullinmax@gmail.com>
2023-07-02 01:23:00 -0400

    allow for db paths

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    DATABASE = 'purrs.sqlite'
+    DATABASE = os.getenv("DATABASE")
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{DATABASE}'
     ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
